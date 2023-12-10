dc005b8faec4da0f68575f8ecefa70bb6ff6dcf7
ERICKxD30 <152240142+ERICKxD30@users.noreply.github.com>
2023-12-10 03:58:26 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyA5ZXggAFIxOm_iu-QEgeM2QVU2CU_LHTo",
+    authDomain: "chat-firecloud-js.firebaseapp.com",
+    projectId: "chat-firecloud-js",
+    storageBucket: "chat-firecloud-js.appspot.com",
+    messagingSenderId: "168988412063",
+    appId: "1:168988412063:web:e4418f55ec048b683f7aaa",
+    measurementId: "G-GWYG42JTS1"
+    });
